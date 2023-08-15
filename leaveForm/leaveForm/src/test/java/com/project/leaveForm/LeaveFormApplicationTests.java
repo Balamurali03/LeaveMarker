bc169ff76edf54683dec2078dd0a34c11162a504
Balamurali03 <balamurali03101997@gmail.com>
2023-08-15 14:42:18 +0530

    1st commit

---
+++ leaveForm/leaveForm/src/test/java/com/project/leaveForm/LeaveFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.leaveForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
